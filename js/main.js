d555d389843fb7f5f2f57c3232714caaaf49f2f8
Nick VanMeter <nickjvm@gmail.com>
2013-09-24 07:55:21 -0700

    Updated scroll function to detect userAgent

--- js/main.js
+++ js/main.js
@@ -1,14 +1,32 @@
 $(function() {
 
   $(window).on("scroll",function() {
-    n = Math.ceil($(window).scrollTop() / 2.5),
-    contentTop = $(window).width() < 480 ? 150 : 360
-	$("#hero").css("-webkit-transform", "translateY(-" + n + "px)");
-      if($(window).scrollTop() > contentTop) {
-        $("header").addClass("shrink");
-      } else {
-        $("header").removeClass("shrink");
-      }
+    
   });
 
 });
+
+if(!navigator.userAgent.match(/(iPod|iPhone|iPad)/)) {
+	window.onscroll = function() {
+		n = Math.ceil($(window).scrollTop() / 2.5),
+		contentTop = $(window).width() < 480 ? 150 : 360;
+		$("#hero").css("-webkit-transform", "translateY(-" + n + "px)");
+		if($(window).scrollTop() > contentTop) {
+			$("header").addClass("shrink");
+		} else {
+			$("header").removeClass("shrink");
+		}
+	}
+} else {
+	document.ontouchmove = function() {
+		n = Math.ceil($(window).scrollTop() / 2.5),
+		contentTop = $(window).width() < 480 ? 150 : 360;
+
+		$("#hero").css("-webkit-transform", "translateY(-" + n + "px)");
+		if($(window).scrollTop() > contentTop) {
+			$("header").addClass("shrink");
+		} else {
+			$("header").removeClass("shrink");
+		}
+	}
+}
