3580f902073730bf61d86af5b689d3e2ec7c5054
Nick VanMeter <nickjvm@gmail.com>
2013-09-25 07:52:42 -0700

    Added reply-to for mail form

--- contact.php
+++ contact.php
@@ -46,7 +46,8 @@ if(count($errors) > 0) {
 		// To send HTML mail, the Content-type header must be set
 		$headers  = 'MIME-Version: 1.0' . "\r\n";
 		$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
-		$headers .= 'From: '.$name.' <'.$email.'>' . "\r\n";
+		$headers .= 'From: '.$name.' <noreply@nickvanmeter.com>' . "\r\n";
+		$headers .= 'Reply-To: '.$email. "\r\n" .
 
 		if(mail($to, $subject, $msg, $headers)) {
 			print_r(json_encode(array("status"=>true)));
