1b4239436c830c0cb68f0976ab87826490119004
Nick VanMeter <nickjvm@gmail.com>
2013-09-24 23:25:54 -0700

    wiring up contact form!

---
+++ contact.php
@@ -0,0 +1,98 @@
+<?php
+
+$required = array("name","email","message");
+
+$errors = array();
+foreach ($_POST as $key => $value) {
+	if(empty($value) || strlen($value) == 0) {
+		array_push($errors,$key);
+	}
+}
+if(isset($_POST['email'])) {
+	if(!check_email_address($_POST['email'])) {
+		array_push($errors,"email");
+	}
+}
+
+if(count($errors) > 0) {
+	print_r(json_encode(array("status"=>false,"code"=>1,"errors"=>$errors)));
+} else { 
+		$name = trim(strip_tags($_POST['name']));
+		$email = trim(strip_tags($_POST['email']));
+		$message = strip_tags(nl2br($_POST['message']),"<br>");
+		
+	  
+	    // Your code here to handle a successful verificatio
+		$to = 'nick@nickvanmeter.com';
+		$subject = "[nickvanmeter.com] ".substr($message,0,10)."...";
+
+		// the message here is HTML, but you could
+		// use plain text in the same manner
+		// this could also be pulled from a template file
+		$msg = "
+					<html>
+					<head></head>
+					<body>
+					<ul>
+						<li><strong>From:</strong> {$name}</li>
+						<li><strong>Phone:</strong> {$phone}</li>
+						<li><strong>Email:</strong> {$email}</li>
+					</ul>
+					<p>
+						Details below:
+					</p>
+					<blockquote>
+						{$message}
+					</blockquote>
+					</body>
+					</html>";
+
+		// To send HTML mail, the Content-type header must be set
+		$headers  = 'MIME-Version: 1.0' . "\r\n";
+		$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+		$headers .= 'From: '.$name.' <{$email}>' . "\r\n";
+
+		if(mail($to, $subject, $msg, $headers)) {
+			print_r(json_encode(array("status"=>true)));
+		} else {
+			print_r(json_encode(array("status"=>false,"code"=>2)));
+		}
+}
+
+function check_email_address($email) {
+  // First, we check that there's one @ symbol, 
+  // and that the lengths are right.
+  if (!ereg("^[^@]{1,64}@[^@]{1,255}$", $email)) {
+    // Email invalid because wrong number of characters 
+    // in one section or wrong number of @ symbols.
+    return false;
+  }
+  // Split it into sections to make life easier
+  $email_array = explode("@", $email);
+  $local_array = explode(".", $email_array[0]);
+  for ($i = 0; $i < sizeof($local_array); $i++) {
+    if
+(!ereg("^(([A-Za-z0-9!#$%&'*+/=?^_`{|}~-][A-Za-z0-9!#$%&
+↪'*+/=?^_`{|}~\.-]{0,63})|(\"[^(\\|\")]{0,62}\"))$",
+$local_array[$i])) {
+      return false;
+    }
+  }
+  // Check if domain is IP. If not, 
+  // it should be valid domain name
+  if (!ereg("^\[?[0-9\.]+\]?$", $email_array[1])) {
+    $domain_array = explode(".", $email_array[1]);
+    if (sizeof($domain_array) < 2) {
+        return false; // Not enough parts to domain
+    }
+    for ($i = 0; $i < sizeof($domain_array); $i++) {
+      if
+(!ereg("^(([A-Za-z0-9][A-Za-z0-9-]{0,61}[A-Za-z0-9])|
+↪([A-Za-z0-9]+))$",
+$domain_array[$i])) {
+        return false;
+      }
+    }
+  }
+  return true;
+}
--- js/main.js
+++ js/main.js
@@ -8,8 +8,89 @@ $(function() {
 	    $("#twitter").fadeIn();
 	});
 
-});
+	$('#email').magnificPopup({
+		type: 'inline',
+		preloader: false,
+		focus: '#name',
 
+		// When elemened is focused, some mobile browsers in some cases zoom in
+		// It looks not nice, so we disable it:
+		callbacks: {
+			beforeOpen: function() {
+			  if($(window).width() < 700) {
+			    this.st.focus = false;
+			  } else {
+			    this.st.focus = '#name';
+			  }
+			},
+			beforeClose:function() {
+				clearForm("#contact");
+				$("#contact").removeClass("thinking");
+				validator.resetForm();
+			}
+		}
+	});
+
+	var validator = $("#contact").validate({
+		errorPlacement:function() {}
+	});
+
+	$("#contact .cancel").on("click",function(e) {
+		e.preventDefault();
+		validator.resetForm();
+		$.magnificPopup.close()
+	});
+
+	$("#contact").on("submit",function(e) {
+		if(!validator.valid()) { 
+			return false;
+		}
+		var $self = $(this);
+		//do some cleanup
+		e.preventDefault();
+		validator.resetForm();
+		$self.addClass("thinking");
+		var data = $(this).serialize();
+		
+		var request = $.ajax({
+			type: "POST",
+			url: "contact.php",
+			data: data
+		})
+
+		request.done(function(response,success) {
+			$self.removeClass("thinking");
+			response = $.parseJSON(response);
+			if(response.status) {
+				//passed validation - got a good response!
+				clearForm("#contact");
+				$.magnificPopup.close();
+				$("#success").css({
+					top:0
+				})
+				setTimeout(function() {
+					$("#success").css({
+						top:-40
+					})
+				},2500);
+
+			} else {
+				if(response.code == 1) {
+					//something went wrong!
+					$.each(response.errors,function() {
+						$("#contact [name='"+this+"']").addClass("error");
+					});
+			 	} else if (response.code == 2) {
+			 		$("#contact .alert-error").text("Something went wrong. Please try again later");
+			 	}
+			}
+		});
+
+	});
+});
+var clearForm = function(selector) {
+	$(selector).find(":input").val("").prop("checked",false);
+}
 if(!navigator.userAgent.match(/(iPod|iPhone|iPad)/)) {
 	window.onscroll = function() {
 		n = Math.ceil($(window).scrollTop() / 2.5),
