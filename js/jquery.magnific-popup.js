f7b7d6311620a4a3216880625e97b3c029b9b03f
nickjvm <nickjvm@gmail.com>
2013-09-25 10:21:51 -0700

    remove overflow:hidden when lightbox is up

--- js/jquery.magnific-popup.js
+++ js/jquery.magnific-popup.js
@@ -356,10 +356,10 @@ MagnificPopup.prototype = {
 
 		if(mfp.fixedContentPos) {
 			if(!mfp.isIE7) {
-				windowStyles.overflow = 'hidden';
+				//windowStyles.overflow = 'hidden';
 			} else {
 				// ie7 double-scroll bug
-				$('body, html').css('overflow', 'hidden');
+				//$('body, html').css('overflow', 'hidden');
 			}
 		}
 
